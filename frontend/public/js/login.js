f447d52a972899bb13babb259fe395832a55fcc7
ssj400 <gafetel2017@gmail.com>
2025-04-02 17:57:25 -0300

    Added loader to the login

--- frontend/public/js/login.js
+++ frontend/public/js/login.js
@@ -1,12 +1,14 @@
 import { loginUser } from "./api.js";
 
 async function handleLogin() {
-
+    let loader = document.querySelector(".loader");
     const nickname = document.getElementById("nickname").value;
     const password = document.getElementById("password").value;
+    loader.classList.remove("hidden")
 
     if (!nickname || !password) {
         document.getElementById("errorMsg").textContent = "Por favor complete todos los campos";
+        loader.classList.add("hidden");
         return;
     }
 
@@ -15,6 +17,8 @@ async function handleLogin() {
     } catch (error) {
         console.error("Error al iniciar sesión:", error);
         document.getElementById("errorMsg").textContent = error.message;
+    } finally {
+        loader.classList.add("hidden");
     }
 }
 
